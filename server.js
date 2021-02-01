0cadf1248abd01258058832ca44bea4cc086f165
Mr.Smart Killer_08 <gowtham.kumar492@gmail.com>
2021-02-01 22:46:37 +0530

    server started

---
+++ server.js
@@ -0,0 +1,52 @@
+const express=require("express");
+const bodyparser=require("body-parser");
+const cors=require("cors");
+let mongodb=require("mongodb");
+
+let app=express();
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(cors());
+
+let ashokIt=mongodb.MongoClient;
+
+app.post("/login",(req,res)=>
+{
+    ashokIt.connect("mongodb+srv://admin:admin@cluster0.abahc.mongodb.net/ecommerce?retryWrites=true&w=majority",(err,conn) =>
+    {
+        if(err) throw err;
+        else
+        {
+            let db=conn.db("ecommerce");
+            db.collection("login_details").find({"uname":req.body.uname,"upwd":req.body.upwd}).toArray((err,my_arr)=>
+            {
+                if(err) throw err;
+                else{
+                    if(my_arr.length>0)
+                    {
+                        res.status(200).json({login:"success"});
+                    }
+                    else
+                    {
+                        res.status(200).json({login:"fail"});
+                    }
+                }
+
+            })
+
+        }
+
+    })
+
+})
+
+//assign port 
+
+let port =process.env.PORT || 8080
+app.listen(port,() =>
+{
+    console.log("server started");
+})
+
+
+
